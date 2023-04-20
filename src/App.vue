69dcf5056bb8412cf08e7ae00c46fae2578c1fe7
Enrique Gonzalez <Enrique.gonzalezalvarezpalencia@lumen.com>
2023-04-20 17:47:10 +0200

    Exercise 3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
-<script>
-import { ref, computed } from "vue";
+<script setup>
+import {ref, computed} from "vue";
 import ProductCard from "./components/ProductCard.vue";
-export default {
-  components: { ProductCard },
-  setup() {
-    const loading = ref(true);
-    const products = ref([]);
-    const numberOfProducts = computed(() => products.value.length);
-    async function fetchProducts() {
-      const res = await fetch("https://dummyjson.com/products?limit=10000");
-      const data = await res.json();
-      products.value = data.products;
-    }
-    fetchProducts().then(() => {
-      loading.value = false;
-    });
-    return { loading, products, numberOfProducts, fetchProducts };
-  },
-};
+
+const loading = ref(true);
+const products = ref([]);
+const numberOfProducts = computed(() => products.value.length);
+
+async function fetchProducts() {
+  const res = await fetch("https://dummyjson.com/products?limit=10000");
+  const data = await res.json();
+  products.value = data.products;
+}
+
+fetchProducts().then(() => {
+  loading.value = false;
+});
 </script>
 
 <template>
@@ -26,10 +23,10 @@ export default {
     <h1 class="text-2xl mb-5">Products ({{ numberOfProducts }})</h1>
     <ul class="flex gap-4 flex-wrap flex-grow justify-center">
       <ProductCard
-        v-for="product in products"
-        :key="product.id"
-        :product="product"
-        class="w-80"
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+          class="w-80"
       />
     </ul>
   </div>
