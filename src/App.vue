af153de4c56cf8794ca2b84ea30d0c3697e2657d
Enrique Gonzalez <Enrique.gonzalezalvarezpalencia@lumen.com>
2023-05-03 15:10:33 +0200

    Exercise 8

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,11 @@
 <script setup>
-import { ref, computed } from "vue";
-import ProductCard from "./components/ProductCard.vue";
-import { useFetch } from "./composables/useFetch";
-import { useSort } from "./composables/useSort";
-import { refDebounced } from "@vueuse/core";
-import { useEscapeKey } from "./composables/useEscKey";
-
-// loading products
-const query = ref("");
-useEscapeKey(() => {
-  query.value = "";
-});
-const queryDebounced = refDebounced(query, 400);
-const url = computed(
-  () =>
-    `https://dummyjson.com/products/search?limit=10000&q=${queryDebounced.value}`
-);
-const { data, loading } = useFetch(url);
-const products = computed(() => data.value?.products || []);
-
-// products meta
-const numberOfProducts = computed(() => products.value.length);
-
-// ordering products
-const orderBy = ref("price");
-const desc = ref(false);
-const { sorted: orderedProducts } = useSort(products, orderBy, desc);
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
 
 <template>
   <div>
-    <h1 class="text-2xl mb-5">Products ({{ numberOfProducts }})</h1>
-    <label class="w-1/3"
-      >Order by
-      <select
-        class="border-2 border-gray-300 rounded w-1/3 p-2"
-        v-model="orderBy"
-      >
-        <option value="price">Price</option>
-        <option value="rating">Rating</option>
-      </select>
-      <input
-        type="text"
-        class="border-2 border-gray-300 rounded ml-2 p-2"
-        placeholder="Search"
-        v-model="query"
-      />
-      <label class="pl-3 w-1/3">
-        <input type="checkbox" v-model="desc" />
-        Descending
-      </label>
-    </label>
-
-    <div v-if="loading">loading...</div>
-    <ul v-else class="flex gap-4 flex-wrap flex-grow justify-center">
-      <ProductCard
-        v-for="product in orderedProducts"
-        :key="product.id"
-        :product="product"
-        class="w-80"
-      />
-    </ul>
+    <h1 class="text-2xl mb-5">Video chulo</h1>
+    <VideoPlayer src="/coverr-dog-on-the-beach-3457-1080p.mp4"/>
   </div>
 </template>
 
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {ref, computed} from "vue";
+
+const props = defineProps(['src']);
+const video = ref(null);
+const paused = ref(true)
+const start = () => {
+  video.value.play();
+  paused.value = false;
+}
+const pause = () => {
+  video.value.pause();
+  paused.value = true;
+}
+</script>
+
+<template>
+  <video class="mb-2" ref="video" loop muted playsinline :src="props.src" width="600"></video>
+  <button v-if="paused" @click="start" class="border-black border rounded p-5">Start</button>
+  <button v-else @click="pause" class="border-black border rounded p-5">Pause</button>
+</template>
